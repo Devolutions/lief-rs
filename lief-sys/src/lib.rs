e00f0fabbc2a70e67de4e5912fdd73b53a87adea
Alexandr Yusuk <aleksandr.yusuk@apriorit.com>
2021-02-18 18:40:41 +0200

    Implemented basic function for interacting with PE file and resources

--- lief-sys/src/lib.rs
+++ lief-sys/src/lib.rs
@@ -1,10 +1,13 @@
 use std::ffi::c_void;
-use std::os::raw::c_char;
+use std::os::raw::{c_char, c_uint};
 
 pub type Binary = *mut c_void;
+pub type ResourceManager = *mut c_void;
 
 extern "C" {
     pub fn Binary_New(path: *const c_char) -> Binary;
     pub fn Binary_Free(this: Binary);
-    pub fn Binary_Print(this: Binary);
+    pub fn Binary_Build(this: Binary) -> c_uint;
+    pub fn Binary_GetResourceManager(this: Binary) -> ResourceManager;
+    pub fn ResourceManager_Free(this: ResourceManager);
 }
--- lief-sys/src/liblief.cpp
+++ lief-sys/src/liblief.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <memory>
-#include <string>
+#include <exception>
 #include "LIEF/PE.hpp"
-
 
 #ifdef _MSC_VER
     #define LIEF_SYS_EXPORT __declspec(dllexport)
@@ -10,28 +9,74 @@
     #define LIEF_SYS_EXPORT __attribute__((dllexport))
 #endif
 
+#define LIEF_SYS_OK 0
+#define LIEF_SYS_BUILD_ERROR 1
+#define LIEF_SYS_SET_ICON_ERROR 2
+#define LIEF_SYS_SET_RCDATA_ERROR 3
+#define LIEF_SYS_SET_STRING_ERROR 4
+
+using namespace LIEF;
+
 typedef struct Binary Binary;
+typedef struct ResourceManager ResourceManager;
 
 extern "C"
 {
-    LIEF_SYS_EXPORT Binary* Binary_New(const char * path)
+    LIEF_SYS_EXPORT Binary* Binary_New(const char* path)
     {
-        std::unique_ptr<LIEF::PE::Binary> unique_ptr = LIEF::PE::Parser::parse(std::string(path));
-        LIEF::PE::Binary* ptr = unique_ptr.release();
-        return reinterpret_cast<Binary*>(ptr);
+        std::unique_ptr<PE::Binary> binary;
+        try {
+            binary = PE::Parser::parse(path);
+        } catch (const std::exception& ex) {
+            return nullptr;
+        }
+
+        return reinterpret_cast<Binary*>(binary.release());
     }
 
     LIEF_SYS_EXPORT void Binary_Free(Binary* _this)
     {
-        LIEF::PE::Binary* binary = reinterpret_cast<LIEF::PE::Binary*>(_this);
+        PE::Binary* binary = reinterpret_cast<PE::Binary*>(_this);
         delete binary;
     }
 
-    LIEF_SYS_EXPORT  void Binary_Print(Binary* _this)
+    LIEF_SYS_EXPORT unsigned int Binary_Build(Binary* _this, const char* path)
     {
-        LIEF::PE::Binary* binary = reinterpret_cast<LIEF::PE::Binary*>(_this);
-        std::cout << *binary << std::endl;
+        PE::Binary* binary = reinterpret_cast<PE::Binary*>(_this);
+        PE::Builder builder = PE::Builder::Builder(binary);
+
+        builder.build_resources(true);
+
+        try {
+            builder.build();
+            builder.write(path);
+        } catch (const std::exception& ex) {
+            return LIEF_SYS_BUILD_ERROR;
+        }
+
+        return LIEF_SYS_OK;
     }
 
+    LIEF_SYS_EXPORT ResourceManager* Binary_GetResourceManager(Binary* _this)
+    {
+        PE::Binary* binary = reinterpret_cast<PE::Binary*>(_this);
+
+        PE::ResourcesManager tmp(nullptr);
+        try {
+            tmp = binary->resources_manager();
+        } catch (std::exception& ex) {
+            return nullptr;
+        }
+
+        PE::ResourcesManager* resourceManager = new PE::ResourcesManager(nullptr);
+        *resourceManager = tmp;
+
+        return reinterpret_cast<ResourceManager*>(resourceManager);
+    }
+
+    LIEF_SYS_EXPORT void ResourceManager_Free(ResourceManager* _this) {
+        PE::ResourcesManager* resourceManager = reinterpret_cast<PE::ResourcesManager*>(_this);
+        delete resourceManager;
+    }
 }
 

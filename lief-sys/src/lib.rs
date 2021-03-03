36280ccee1e297698d7c092b24e925328bf66c37
Alexandr Yusuk <aleksandr.yusuk@apriorit.com>
2021-03-03 17:06:17 +0200

    Coveraged code with integration tests

--- lief-sys/src/lib.rs
+++ lief-sys/src/lib.rs
@@ -13,9 +13,21 @@ extern "C" {
     pub fn SetRcData(
         this: ResourceManager,
         data: *const u8,
-        data_size: usize,
+        data_len: usize,
         resource_id: u32,
     ) -> c_uint;
+    pub fn GetRcData(this: ResourceManager, resource_id: u32, rcdata_len: *mut usize) -> *const u8;
+    pub fn DeallocateRcData(rcdata: *const u8);
     pub fn SetString(this: ResourceManager, string: *const u16, resource_id: u32) -> c_uint;
-    pub fn ReplaceIcon(this: ResourceManager, data: *const u8, data_size: usize) -> c_uint;
+    pub fn GetString(this: ResourceManager, resource_id: u32, string_len: *mut usize)
+        -> *const u16;
+    pub fn DeallocateString(string: *const u16);
+    pub fn ReplaceIcon(this: ResourceManager, data: *const u8, data_len: usize) -> c_uint;
+    pub fn GetIcon(
+        this: ResourceManager,
+        width: u32,
+        height: u32,
+        pixels_data_len: *mut usize,
+    ) -> *const u8;
+    pub fn DeallocateIcon(pixels: *const u8);
 }
--- lief-sys/src/liblief.cpp
+++ lief-sys/src/liblief.cpp
@@ -1,5 +1,7 @@
+#include <cmath>
 #include <memory>
 #include <exception>
+#include <stdexcept>
 #include <vector>
 #include "LIEF/PE.hpp"
 
@@ -17,11 +19,14 @@ enum class LIEF_SYS_STATUS: unsigned  int {
     SET_ICON_ERROR
 };
 
+const uint32_t gMaxStringsCount = 16;
+
 using namespace LIEF;
 
 typedef struct Binary Binary;
 typedef struct ResourceManager ResourceManager;
 
+PE::ResourceIcon CreateIconFromRawData(const uint8_t* data, size_t data_size);
 void DeleteRootNodeChilds(PE::ResourceNode& root, uint32_t dir_node_id);
 
 extern "C"
@@ -43,14 +48,14 @@ extern "C"
 
     LIEF_SYS_EXPORT void Binary_Free(Binary* _this)
     {
-        PE::Binary* binary = reinterpret_cast<PE::Binary*>(_this);
+        auto* binary = reinterpret_cast<PE::Binary*>(_this);
         delete binary;
     }
 
     LIEF_SYS_EXPORT unsigned int Binary_Build(Binary* _this, const char* path)
     {
-        PE::Binary* binary = reinterpret_cast<PE::Binary*>(_this);
-        PE::Builder builder = PE::Builder::Builder(binary);
+        auto* binary = reinterpret_cast<PE::Binary*>(_this);
+        PE::Builder builder = PE::Builder(binary);
 
         builder.build_resources(true);
 
@@ -69,7 +74,7 @@ extern "C"
 
     LIEF_SYS_EXPORT ResourceManager* Binary_GetResourceManager(Binary* _this)
     {
-        PE::Binary* binary = reinterpret_cast<PE::Binary*>(_this);
+        auto* binary = reinterpret_cast<PE::Binary*>(_this);
 
         PE::ResourcesManager* resourceManager = nullptr;
         PE::ResourcesManager tmp(nullptr);
@@ -90,19 +95,18 @@ extern "C"
     }
 
     LIEF_SYS_EXPORT void ResourceManager_Free(ResourceManager* _this) {
-        PE::ResourcesManager* resourceManager = reinterpret_cast<PE::ResourcesManager*>(_this);
-        delete resourceManager;
+        auto* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
+        delete resources_manager;
     }
 
     LIEF_SYS_EXPORT unsigned int SetRcData(ResourceManager* _this, const uint8_t* data, size_t data_size, uint32_t resource_id) {
-        PE::ResourcesManager* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
+        auto* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
 
         try
         {
-            PE::ResourceNode &rc_data_root = resources_manager->get_node_type(PE::RESOURCE_TYPES::RCDATA);
+            PE::ResourceNode& rcdata_root = resources_manager->get_node_type(PE::RESOURCE_TYPES::RCDATA);
 
-            DeleteRootNodeChilds(rc_data_root, resource_id);
-
+            DeleteRootNodeChilds(rcdata_root, resource_id);
 
             PE::ResourceData rcdata_data_node;
             PE::ResourceDirectory rcdata_dir_node;
@@ -119,7 +123,7 @@ extern "C"
             rcdata_dir_node.id(resource_id);
             rcdata_dir_node.numberof_id_entries(1);
 
-            rc_data_root.add_child(rcdata_dir_node);
+            rcdata_root.add_child(rcdata_dir_node);
         }
         catch (const std::exception& ex)
         {
@@ -129,38 +133,86 @@ extern "C"
         return static_cast<unsigned int>(LIEF_SYS_STATUS::OK);
     }
 
-    LIEF_SYS_EXPORT unsigned int SetString(ResourceManager* _this, const uint16_t* string_data, const uint32_t resource_id) {
-        const uint32_t max_strings_count = 16;
+    LIEF_SYS_EXPORT uint8_t* GetRcData(ResourceManager* _this, uint32_t resource_id, size_t* rcdata_size) {
+        auto* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
 
-        PE::ResourcesManager* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
+        uint8_t* rcdata = nullptr;
+        try
+        {
+            PE::ResourceNode& rcdata_root = resources_manager->get_node_type(PE::RESOURCE_TYPES::RCDATA);
+            auto childs = rcdata_root.childs();
+
+            auto&& rcdata_dir_node = std::find_if(
+                    std::cbegin(childs),
+                    std::cend(childs),
+                    [&] (const auto& node) {
+                        return node.id() == resource_id;
+                    });
+
+            if(rcdata_dir_node == std::cend(childs))
+                return nullptr;
+
+            auto dir_childs = rcdata_dir_node->childs();
+            if(dir_childs.size() == 0)
+                return nullptr;
+
+            const auto* rcdata_data_node = static_cast<PE::ResourceData*>(&dir_childs[0]);
+
+            if (rcdata_data_node == nullptr)
+                return nullptr;
+
+            const auto& content = rcdata_data_node->content();
+            if(content.size() == 0)
+                return nullptr;
+
+            rcdata = new uint8_t[content.size()];
+
+            std::copy(std::cbegin(content), std::cend(content), rcdata);
+            *rcdata_size = content.size();
+        }
+        catch(const std::exception& ex)
+        {
+            return nullptr;
+        }
+
+        return rcdata;
+    }
+
+    LIEF_SYS_EXPORT void DeallocateRcData(const uint8_t* rcdata){
+        delete[] rcdata;
+    }
+
+    LIEF_SYS_EXPORT unsigned int SetString(ResourceManager* _this, const uint16_t* string_data, const uint32_t resource_id) {
+        auto* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
+
         try
         {
             PE::ResourceNode& string_table_root  = resources_manager->get_node_type(PE::RESOURCE_TYPES::STRING);
 
             std::u16string string = std::u16string(reinterpret_cast<char16_t const*>(string_data));
 
-            DeleteRootNodeChilds(string_table_root, resource_id % max_strings_count);
+            DeleteRootNodeChilds(string_table_root, resource_id % gMaxStringsCount);
 
             PE::ResourceData string_table_data_node;
             PE::ResourceDirectory string_table_dir_node;
 
-            std::vector<uint8_t> content(max_strings_count * 2, 0);
+            std::vector<uint8_t> content(gMaxStringsCount * 2, 0);
 
-            size_t blockId = 2 * (static_cast<size_t>(resource_id) % max_strings_count);
+            size_t blockId = 2 * (static_cast<size_t>(resource_id) % gMaxStringsCount);
             content[blockId] = string.size();
 
-            const uint8_t* begin = reinterpret_cast<uint8_t const*>(string.c_str());
-            const uint8_t* end = reinterpret_cast<uint8_t const*>(string.c_str() + string.size());
+            const auto* begin = reinterpret_cast<uint8_t const*>(string.c_str());
+            const auto* end = reinterpret_cast<uint8_t const*>(string.c_str() + string.size());
 
             const size_t offset = std::distance(content.data(), content.data() + blockId + 2);
-            std::vector<uint8_t>::const_iterator pos = content.cbegin() + offset;
+            auto pos = content.cbegin() + offset;
 
             content.insert(pos, begin, end);
 
             string_table_data_node.id(static_cast<uint32_t>(PE::RESOURCE_LANGS::LANG_ENGLISH));
             string_table_data_node.content(content);
 
-            string_table_dir_node.id(resource_id % max_strings_count);
+            string_table_dir_node.id(static_cast<uint32_t>(std::ceil(float(resource_id) / gMaxStringsCount)));
             string_table_dir_node.add_child(string_table_data_node);
             string_table_dir_node.numberof_id_entries(1);
 
@@ -174,39 +226,95 @@ extern "C"
         return static_cast<unsigned int>(LIEF_SYS_STATUS::OK);
     }
 
-    LIEF_SYS_EXPORT unsigned int ReplaceIcon(ResourceManager* _this, uint8_t* data, size_t data_size) {
+    LIEF_SYS_EXPORT uint16_t* GetString(ResourceManager* _this, const uint32_t resource_id,  size_t* const string_size) {
+        auto* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
+
+        uint16_t* string = nullptr;
+        try
+        {
+            PE::ResourceNode& string_table_root  = resources_manager->get_node_type(PE::RESOURCE_TYPES::STRING);
+            auto root_childs = string_table_root.childs();
+
+            auto string_dir_id = static_cast<uint32_t>(std::ceil(float(resource_id) / gMaxStringsCount));
+
+            auto&& string_table_dir_node = std::find_if(
+                    std::cbegin(root_childs),
+                    std::cend(root_childs),
+                    [&](const auto& node) {
+                        return node.id() == string_dir_id;
+                    });
+
+            if(string_table_dir_node == std::cend(root_childs))
+                return nullptr;
+
+            auto dir_childs = string_table_dir_node->childs();
+            if(dir_childs.size() == 0)
+                return nullptr;
+
+            const auto* string_table_data_node = static_cast<PE::ResourceData*>(&dir_childs[0]);
+            if(string_table_data_node == nullptr)
+                return nullptr;
+
+            const auto& content = string_table_data_node->content();
+            if(content.size() < 2 * gMaxStringsCount)
+                return nullptr;
+
+            size_t blockId = 2 * (static_cast<uint32_t>(resource_id) % gMaxStringsCount);
+
+            *string_size = content[blockId];
+
+            const size_t offset = std::distance(content.data(), content.data() + blockId + 2);
+            std::u16string u16string(reinterpret_cast<const uint16_t*>(content.data() + offset),
+                                        reinterpret_cast<const uint16_t*>(content.data() + offset +  2 * *string_size));
+
+            string = new uint16_t[*string_size];
+
+            std::copy(std::cbegin(u16string), std::cend(u16string), string);
+        }
+        catch(const std::exception& ex)
+        {
+            return nullptr;
+        }
+
+        return string;
+    }
+
+    LIEF_SYS_EXPORT void DeallocateString(const uint16_t* string) {
+        delete[] string;
+    }
+
+    LIEF_SYS_EXPORT unsigned int ReplaceIcon(ResourceManager* _this, const uint8_t* data, size_t data_size) {
         PE::ResourcesManager* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
 
         try
         {
-            const PE::pe_icon_header* icon_header = reinterpret_cast<const PE::pe_icon_header*>(data + sizeof(PE::pe_resource_icon_dir));
-            PE::ResourceIcon icon(icon_header);
+            if (!resources_manager->has_icons())
+                return static_cast<unsigned int>(LIEF_SYS_STATUS::SET_ICON_ERROR);
 
-            std::vector<uint8_t> pixels = { data + icon_header->offset, data + icon_header->offset + icon_header->size };
-            icon.pixels(pixels);
+            PE::ResourceIcon icon = CreateIconFromRawData(data, data_size);
 
             PE::ResourceNode& icon_group_root = resources_manager->get_node_type(PE::RESOURCE_TYPES::GROUP_ICON);
             PE::ResourceNode& icon_root = resources_manager->get_node_type(PE::RESOURCE_TYPES::ICON);
 
             icon_group_root.sort_by_id();
-            for(PE::ResourceNode& grp_icon_dir_node: icon_group_root.childs()) {
+            for(auto& grp_icon_dir_node: icon_group_root.childs()) {
                 grp_icon_dir_node.sort_by_id();
 
-                for(PE::ResourceNode& grp_icon_data_node: grp_icon_dir_node.childs()) {
-                    PE::ResourceData* icon_group_res_data = static_cast<PE::ResourceData*>(&grp_icon_data_node);
+                for(auto& grp_icon_data_node: grp_icon_dir_node.childs()) {
+                    auto* icon_group_res_data = static_cast<PE::ResourceData*>(&grp_icon_data_node);
 
-                    std::vector<uint8_t> icon_group_content = icon_group_res_data->content();
-                    PE::pe_resource_icon_dir* icon_group_header = reinterpret_cast<PE::pe_resource_icon_dir*>(icon_group_content.data());
+                    auto icon_group_content = icon_group_res_data->content();
+                    auto* icon_group_header = reinterpret_cast<PE::pe_resource_icon_dir*>(icon_group_content.data());
 
                     for(size_t i = 0; i < icon_group_header->count; i++) {
-                        PE::pe_resource_icon_group* icon_header = reinterpret_cast<PE::pe_resource_icon_group*>(
+                        auto* icon_header = reinterpret_cast<PE::pe_resource_icon_group*>(
                                     icon_group_content.data() +
                                     sizeof(PE::pe_resource_icon_dir) +
                                     i * sizeof(PE::pe_resource_icon_group));
 
                         if(icon_header != nullptr
-                            && icon_header->width == icon.width()
-                            && icon_header->height == icon.height())
+                            && (icon_header->width == icon.width())
+                            && (icon_header->height == icon.height()))
                         {
                             icon_header->color_count = icon.color_count();
                             icon_header->reserved = icon.reserved();
@@ -216,12 +324,14 @@ extern "C"
 
                             icon.id(icon_header->ID);
 
-                            icon_root.delete_child(icon_header->ID);
-                            PE::ResourceDirectory icon_res_dir;
-                            icon_res_dir.id(icon.id());
+                            DeleteRootNodeChilds(icon_root, icon_header->ID);
 
                             PE::ResourceData icon_res_data{ icon.pixels(), 0 };
                             icon_res_data.id(static_cast<int>(icon.sublang()) << 10 | static_cast<int>(icon.lang()));
+
+                            PE::ResourceDirectory icon_res_dir;
+                            icon_res_dir.id(icon.id());
+                            icon_res_dir.add_child(icon_res_data);
 
                             icon_root.add_child(icon_res_dir);
                         }
@@ -236,25 +346,88 @@ extern "C"
 
         return static_cast<unsigned int>(LIEF_SYS_STATUS::OK);
     }
+
+    LIEF_SYS_EXPORT uint8_t* GetIcon(ResourceManager* _this, uint32_t width, uint32_t height, size_t* pixels_data_size) {
+        auto* resources_manager = reinterpret_cast<PE::ResourcesManager*>(_this);
+
+        uint8_t* icon_pixels = nullptr;
+        try
+        {
+            if(!resources_manager->has_icons())
+                return nullptr;
+
+            auto icons = resources_manager->icons();
+
+            auto icon_width = static_cast<uint8_t>(width);
+            auto icon_height = static_cast<uint8_t>(height);
+
+            auto&& icon = std::find_if(
+                    std::cbegin(icons),
+                    std::cend(icons),
+                    [&](const auto& icon) {
+                        return icon.width() == icon_width && icon.height() ==  icon_height;
+                    });
+
+            if(icon == std::cend(icons))
+                return nullptr;
+
+            auto pixels = icon->pixels();
+            if(pixels.size() == 0)
+                return nullptr;
+
+            icon_pixels = new uint8_t[pixels.size()];
+
+            std::copy(std::cbegin(pixels), std::cend(pixels), icon_pixels);
+            *pixels_data_size = pixels.size();
+        }
+        catch (const std::exception& ex)
+        {
+            return nullptr;
+        }
+
+        return icon_pixels;
+    }
+
+    LIEF_SYS_EXPORT void DeallocateIcon(const uint8_t* icon_data) {
+        delete[] icon_data;
+    }
 }
+
+PE::ResourceIcon CreateIconFromRawData(const uint8_t* data, size_t data_size) {
+    if(data_size < sizeof(PE::pe_resource_icon_dir))
+        throw std::out_of_range("Icon data size is less than pe_resource_icon_dir size");
+
+
+    const auto* icon_header = reinterpret_cast<const PE::pe_icon_header*>(data + sizeof(PE::pe_resource_icon_dir));
+    PE::ResourceIcon icon{icon_header};
+
+    if(data_size < icon_header->offset + icon_header->size)
+        throw std::out_of_range("Icon data size is less than icon_header offset + icon_header size");
+
+    std::vector<uint8_t> pixels = { data + icon_header->offset, data + icon_header->offset + icon_header->size };
+    icon.pixels(pixels);
+
+    return icon;
+}
+
 void DeleteRootNodeChilds(PE::ResourceNode& root, uint32_t dir_node_id) {
     auto childs = root.childs();
 
     auto&& dir_node = std::find_if(
-            std::begin(childs),
-            std::end(childs),
-            [&](const PE::ResourceNode& node) {
+            std::cbegin(childs),
+            std::cend(childs),
+            [&](const auto& node) {
                 return node.id() == dir_node_id;
             });
 
-    if(dir_node == std::end(childs)) 
+    if(dir_node == std::cend(childs))
         return;
 
     auto data_nodes = dir_node->childs();
     std::for_each(
             std::begin(data_nodes),
             std::end(data_nodes),
-            [&](const PE::ResourceNode& data_node)
+            [&](const auto& data_node)
             {
                 dir_node->delete_child(data_node);
             });

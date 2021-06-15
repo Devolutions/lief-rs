63391f6e43ffd04c6834485129e27ab5ea000ed1
Marc-André Moreau <mamoreau@devolutions.net>
2021-06-15 10:10:27 -0400

    fix MSVC static runtime linking (lots of fun)

--- lief-sys/build.rs
+++ lief-sys/build.rs
@@ -12,14 +12,23 @@ fn main() {
 
     let install_dir = Config::new(".")
         .define("BUILD_SHARED_LIBS", "OFF")
-        .static_crt(false)
+        .static_crt(true)
         .no_build_target(true)
         .build();
 
     // main LIEF library
+    let mut lief_lib_name = "LIEF";
+    if cfg!(windows) {
+        lief_lib_name = if profile == "debug" {
+            "LIEFMTd"
+        } else {
+            "LIEFMT"
+        };
+    }
+
     let lief_lib_path = install_dir.join("build").join("LIEF").join("lib");
     println!("cargo:rustc-link-search=native={}", lief_lib_path.to_str().unwrap());
-    println!("cargo:rustc-link-lib=static=LIEF");
+    println!("cargo:rustc-link-lib=static={}", lief_lib_name);
 
     // lief-sys library
     let mut lib_path = install_dir.join("build").join("lib");
@@ -36,6 +45,13 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=stdc++");
     } else if target.contains("apple") {
         println!("cargo:rustc-link-lib=dylib=c++");
+    }
+
+    if cfg!(windows) {
+        if profile == "debug" {
+            // ugly hack to force linking the debug runtime
+            println!("cargo:rustc-link-lib={}", "libcmtd");
+        }
     }
 
     println!("cargo:root={}", install_dir.to_str().unwrap());

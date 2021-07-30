2e329785b259226dbba8f8c3c69e38fb92a4b984
Alexandr Yusuk <aleksandr.yusuk@apriorit.com>
2021-07-30 16:48:46 +0300

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -503,11 +503,11 @@ fn cresult_into_lief_result<T>(cresult: CResult<T>) -> LiefResult<T> {
         let message = message.to_str()
             .map_err(|err| LiefError::Other {
                 description: format!("Failed to convert CStr error message to &str: {}", err),
-            })?
-            .to_owned();
+            });
 
         unsafe { lief::DeallocateMessage(cresult.message); }
-        Err(LiefError::CError(message))
+
+        Err(LiefError::CError(message?.to_owned()))
     }
 }
 
